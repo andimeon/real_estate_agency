01b8765654cfe73e22e7b2ea709f763c299a81a4
andimeon <andimeon@gmail.com>
2020-05-19 18:54:28 +0900

    Create new model Claim

--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -25,3 +26,9 @@ class Flat(models.Model):
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+
+class Claim(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Кто жаловался", blank=True, null=True)
+    rooms_number = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name="Номер комнаты")
+    claim_text = models.TextField("Текст жалобы", blank=True, null=True)
