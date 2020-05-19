fc5e19206ffd4f36bc452047a1bac22738ee9cba
andimeon <andimeon@gmail.com>
2020-05-19 18:29:17 +0900

    Add field Flat.new_building

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Flat
 
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'town_district', 'address', 'owner']
     readonly_fields = ['created_at']
