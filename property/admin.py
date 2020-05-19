989b7cb909ead31e03caad7e914abbc8f254e461
andimeon <andimeon@gmail.com>
2020-05-19 18:37:45 +0900

    add fields on Flat Admin

--- property/admin.py
+++ property/admin.py
@@ -6,6 +6,8 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'town_district', 'address', 'owner']
     readonly_fields = ['created_at']
+    list_display = ['address', 'town', 'new_building', 'construction_year', 'price']
+    list_editable = ['new_building']
 
 
 admin.site.register(Flat, FlatAdmin)
