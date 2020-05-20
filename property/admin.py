ce899cabef856ef894e85c2bce6f47aa8a578342
andimeon <andimeon@gmail.com>
2020-05-20 11:04:56 +0900

    displayed phone number

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,7 @@ from .models import Flat, Claim
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'town_district', 'address', 'owner']
     readonly_fields = ['created_at']
-    list_display = ['address', 'town', 'new_building', 'construction_year', 'price']
+    list_display = ['address', 'owner_phone_pure', 'town', 'new_building', 'construction_year', 'price']
     list_editable = ['new_building']
     list_filter = ['new_building', 'has_balcony', 'active']
     raw_id_fields = ['liked_by']
